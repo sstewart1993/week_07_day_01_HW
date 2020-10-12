48a1e11c29640b1c24142acd570e4ff5e2892264
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-5.local>
2020-10-12 15:30:54 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li
+      v-for="todo in todos">
+        <span>{{ todo.name }}</span>
+        <span>{{ todo.priority }}</span>
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="saveNewTodo">
+      <label for="new-todo">Add New Task</label>
+      <input type="text" name="new-todo" id="new-todo" v-model="newTodo"/>
+      <input type="text" name="new-priority" id="new-priority" v-model="newPriority">
+      <input type="submit" value="Save New Task"/>
+    </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      todos: [
+      {name: "Buy shopping: ", priority: " high"},
+      {name: "Clean bathroom: ", priority: " low"},
+      {name:"Car's MOT: ", priority: " Super Duper High"}
+      ],      
+    newTodo: "",
+    newPriority: ""
+    }
+  },
+  methods:{
+    saveNewTodo: function(){
+      this.todos.push({
+        name: this.newTodo,
+        priority: this.newPriority
+
+      })
+      this.newTodo=""
+      this.newPriority=""
+    }
   }
 }
 </script>
